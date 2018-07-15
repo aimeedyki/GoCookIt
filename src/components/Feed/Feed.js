e5aba176158c4dc0367dd1f327d4798f1240c844
aimeedyki <akuwudike.a@gmail.com>
2018-07-15 15:22:55 +0100

    feat(home page): creat home page and navigation
    [Finishes #158940936]

---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hey feed</Text>
+      </View>
+    );
+  }
+}
+
---
+++ src/components/Home/Home.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Font } from 'expo';
+
+export default class Home extends React.Component {
+  state = {
+    ready: false,
+  }
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Apple': require('../../../assets/fonts/Apple-Butter.ttf')
+    })
+    this.setState(() => ({ ready: true }));
+  }
+  render() {
+    const { navigate } = this.props.navigation;
+    if (!this.state.ready) return <Text>Loading...</Text>
+    return (
+      <View style={styles.container}>
+        <Image
+          style={{ marginRight: 25, marginBottom: 50 }}
+          source={require('../../../assets/images/logo3.png')}
+        />
+        <Text style={styles.welcome}>Welcome!</Text>
+        <View >
+          <TouchableOpacity 
+          style={styles.button} 
+          onPress={() => { navigate('Feed') }}
+          >
+            <Text style={styles.go}>GoCookIt</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E26664',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcome: {
+    marginBottom: 40,
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#Eee6d7'
+  },
+  button: {
+    backgroundColor: '#3F1A18',
+    padding: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20
+  },
+  go: {
+    color: '#Eee6d7',
+    fontFamily: 'Apple',
+    fontSize: 45,
+    fontWeight: 'bold',
+  }
+});
